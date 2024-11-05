48a41c7837034e57bb1ee75c483e04928d38ebad
saramargarido <saramargarido@gmail.com>
2024-11-05 18:12:02 +0100

    login page with tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,22 @@
 import config from '@rocketseat/eslint-config/react.mjs'
 
-export default config
+export default [
+  ...config,
+  {
+    rules: {
+    ...config.rules,
+    "react/self-closing-comp": ["error", {
+    "component": true,
+    "html": false
+    }],
+    // '@stylistic/js/max-len': ['error', { 
+    //   "code": 80,
+    //   "ignoreComments": true, 
+    //   "ignoreStrings": true,
+    //   "ignoreUrls": true 
+    // }],
+  }
+}
+]
+
+
