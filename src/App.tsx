740baabe1a5884738af9b2b6bdf81794b06f5a73
saramargarido <saramargarido@gmail.com>
2024-11-05 17:05:14 +0100

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { Button } from './components/ui/button'
 import './global.css'
 
 export function App() {
-
   return (
     <>
-      <div className='flex justify-center items-center h-screen'>
+      <div className="flex justify-center items-center h-screen">
         <Button>Enviar</Button>
       </div>
     </>
   )
 }
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import react from '@vitejs/plugin-react'
-import path from "node:path"
+import path from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   resolve: {
+  resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
