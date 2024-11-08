33da314da37d6d18ffccfda6b1e37ad1a26a64e2
saramargarido <saramargarido@gmail.com>
2024-11-08 21:08:08 +0100

    sign-in with react query

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
 import { Label } from '@radix-ui/react-label'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '../../api/sign-in'
 import { Button } from '../../components/ui/button'
 import { Input } from '../../components/ui/input'
 
@@ -22,9 +24,16 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
-  function handleSignIn(data: SignInForm) {
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+
+  })
+
+  async function handleSignIn(data: SignInForm) {
     console.log(data)
     try {
+      await authenticate({ email: data.email })
+
       toast.success('Enviamos um link de autenticação para seu e-mail', {
         action: {
           label: 'Reenviar',
