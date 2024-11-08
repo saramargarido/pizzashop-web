01d1e37c37901c2f0c060f1648df0c6260f854a7
saramargarido <saramargarido@gmail.com>
2024-11-08 21:16:53 +0100

    register a restaurant

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,10 +1,12 @@
 import { Label } from '@radix-ui/react-label'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { registerRestaurant } from '../../api/register-restaurant'
 import { Button } from '../../components/ui/button'
 import { Input } from '../../components/ui/input'
 
@@ -27,13 +29,23 @@ export function SignUp() {
     formState: { isSubmitting },
   } = useForm<SignUpForm>()
 
-  function handleSignUp(data: SignUpForm) {
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  })
+
+  async function handleSignUp(data: SignUpForm) {
     console.log(data)
     try {
+      await registerRestaurantFn({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone,
+      })
       toast.success('Restaurante cadastrado com sucesso', {
         action: {
           label: 'Login',
-          onClick: () => { navigate('/sign-in') },
+          onClick: () => { navigate(`/sign-in?email=${data.email}`) },
         },
       })
     } catch {
