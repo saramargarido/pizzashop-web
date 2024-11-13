8d89149639c3fd99766be788daa385fb439cc963
saramargarido <saramargarido@gmail.com>
2024-11-13 12:49:46 +0100

    table skeleton

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -8,6 +8,7 @@ import { Pagination } from '../../../components/pagination'
 import { Table, TableBody, TableHead, TableHeader, TableRow } from '../../../components/ui/table'
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { OrderTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
@@ -21,7 +22,7 @@ export function Orders() {
     .transform(page => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () => getOrders({
       pageIndex,
@@ -66,6 +67,7 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
                 {result && result.orders.map((order) => (
                   <OrderTableRow key={order.orderId} order={order} />
                 ))}
