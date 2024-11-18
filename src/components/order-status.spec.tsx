8b8be68601ff8fb6a1836b9b517c19d04694f974
saramargarido <saramargarido@gmail.com>
2024-11-18 22:14:07 +0100

    pagination tests

--- src/components/order-status.spec.tsx
+++ src/components/order-status.spec.tsx
@@ -2,7 +2,6 @@ import { render } from '@testing-library/react'
 
 import { OrderStatus } from './order-status'
 
-// eslint-disable-next-line no-undef
 describe('Order Status', () => {
   it('should display the right text when order status is pending', () => {
     const wrapper = render(<OrderStatus status="pending" />)
