f2e5fac4f8cdfdab0bbde4172b44e0b88f587e2c
saramargarido <saramargarido@gmail.com>
2024-11-20 13:54:50 +0100

    setup msw

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { enableMSW } from './api/mocks/index.ts'
 import { App } from './app.tsx'
 
-createRoot(document.getElementById('root')!).render(
+enableMSW().then(() => {
+  createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)})
