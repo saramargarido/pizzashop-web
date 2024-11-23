fcb6cde2ada4f4b923e2c77fa1c000d0b84765ea
saramargarido <saramargarido@gmail.com>
2024-11-23 22:09:27 +0100

    test e2e sign-in

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,36 @@
+import { expect, test } from '@playwright/test';
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' });
+
+  await page.getByLabel('Seu e-mail').fill('janedoe@mail.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Enviamos um link de autenticação para seu e-mail')
+
+  expect(toast).toBeVisible()
+
+  // await page.waitForTimeout(2000)
+});
+
+test('sign with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' });
+
+  await page.getByLabel('Seu e-mail').fill('wrong@mail.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Credenciais inválidas')
+
+  expect(toast).toBeVisible()
+
+  // await page.waitForTimeout(2000)
+});
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' });
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+  // await page.waitForTimeout(2000)
+});
