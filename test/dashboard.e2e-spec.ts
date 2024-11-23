ab65d362ccec0917ba2e480b164289f75f2304e5
saramargarido <saramargarido@gmail.com>
2024-11-23 23:16:26 +0100

    test e2e orders list

--- test/dashboard.e2e-spec.ts
+++ test/dashboard.e2e-spec.ts
@@ -5,25 +5,25 @@ test('display day orders amount metric', async ({ page }) => {
 
   expect(page.getByText('20', { exact: true })).toBeVisible()
   expect(page.getByText('-5% em relação a ontem')).toBeVisible()
-})
+});
 
 test('display month orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' });
 
   expect(page.getByText('120', { exact: true })).toBeVisible()
   expect(page.getByText('-23% em relação ao mês passado')).toBeVisible()
-})
+});
 
 test('display month canceled orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' });
 
   expect(page.getByText('12', { exact: true })).toBeVisible()
   expect(page.getByText('-12% em relação ao mês passado')).toBeVisible()
-})
+});
 
 test('display month revenue metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' });
 
   expect(page.getByText('R$ 1.273,39')).toBeVisible()
   expect(page.getByText('+123% em relação ao mês')).toBeVisible()
-})
+});
